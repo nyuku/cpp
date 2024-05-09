1046a8b4690b2b7f99806c1c51e1b3ab3a538f6d
Nyuk <85057677+nyuku@users.noreply.github.com>
2024-05-09 18:20:24 +0200

    Add files via upload
    exercice en plus

---
+++ cpp00/ex02/Account.cpp
@@ -0,0 +1,194 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angnguye <angnguye@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/08 22:58:39 by angnguye          #+#    #+#             */
+/*   Updated: 2024/05/09 17:45:01 by angnguye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <iostream>
+# include <iomanip>
+# include <ctime>
+
+# include "Account.hpp"
+
+//init des infos STATIC general en dehors de la classe
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+static void	_displayTimestamp( void )
+{
+	const time_t	now = time(0);//temps actuel en secondes
+	tm				*ltm = localtime(&now);//struct qui contient le temps décomposé
+
+	std::cout << "[";
+    std::cout << 1900 + ltm->tm_year; //commence a 0 en 1900, 2024 = 124
+    std::cout << std::setfill('0') << std::setw(2) << ltm->tm_mon + 1; // +1 ,janvier = 0;
+    std::cout << std::setfill('0') << std::setw(2) << ltm->tm_mday;
+    std::cout << "_";
+    std::cout << std::setfill('0') << std::setw(2) << ltm->tm_hour;
+    std::cout << std::setfill('0') << std::setw(2) << ltm->tm_min;
+    std::cout << std::setfill('0') << std::setw(2) << ltm->tm_sec;
+    std::cout << "] ";
+}
+
+//---------------------------- CONSTRUCTOR - DESTRUCTOR ----------------------------
+//	Account( int initial_deposit );
+//	~Account( void );
+	
+//render:
+	//[19920104_091532] index:0;amount:42;created
+//variables:
+	// int				_amount;
+	// int				_accountIndex;
+	// int				_nbDeposits;
+	// int				_nbWithdrawals;
+Account::Account( int initial_deposit ) : _amount(initial_deposit)//++total amount, ++ nbAccount
+{
+	_accountIndex = _nbAccounts;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "amount:" << initial_deposit << ";";
+	std:: cout << "created" << std::endl;
+	// ++ static
+	_totalAmount+=initial_deposit;
+	_nbAccounts++;
+}
+//render : [19920104_091532] index:0;amount:47;closed
+Account::~Account( void )
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "amount:" << _amount << ";";
+	std::cout << "closed" << std::endl;
+}
+
+//---------------------------------- GETTERS -------------------------------------------
+// static int	getNbAccounts( void );
+// static int	getTotalAmount( void );
+// static int	getNbDeposits( void );
+// static int	getNbWithdrawals( void );
+int Account::getNbAccounts( void )
+{
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount( void )
+{
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits( void )
+{
+	return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals( void )
+{
+	return (_totalNbWithdrawals);
+}
+
+//----------------------------------- PRINT - DISPLAY----------------------------------
+// static void	displayAccountsInfos( void );
+// void	displayStatus( void ) const;
+
+//RENDER general, variable static
+//	[19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
+void	Account::displayAccountsInfos( void )
+{
+	_displayTimestamp();
+	std::cout << "accounts:" << getNbAccounts() << ";";
+	std::cout << "total:" << getTotalAmount() << ";";
+	std::cout << "deposits:" << getNbDeposits() << ";";
+	std::cout << "withdrawals:" << getNbWithdrawals();
+	std::cout << std::endl;
+}
+//Render pour un account, variable privé: 
+//	[19920104_091532] index:0;amount:42;deposits:0;withdrawals:0
+void	Account::displayStatus( void ) const
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "amount:" << _amount << ";";
+	std::cout << "deposits:" << _nbDeposits << ";";
+	std::cout << "withdrawals:" << _nbWithdrawals << std::endl;
+}
+
+//----------------------------------- PRINT - Update----------------------------------
+// void	makeDeposit( int deposit );
+// bool	makeWithdrawal( int withdrawal );
+
+// Render:
+//	[19920104_091532] index:0;p_amount:42;deposit:5;amount:47;nb_deposits:1
+void	Account::makeDeposit( int deposit )//++ amoubt, ++totalAmoubt, ++nbDeposit, ++ totalNbDeposit 
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "p_amount:" << _amount << ";";
+	std::cout << "deposit:" << deposit << ";";
+	_amount += deposit;
+	_totalAmount += deposit;
+	std::cout << "amount:" << _amount << ";";
+	_nbDeposits++;
+	_totalNbDeposits++;
+	std::cout << "nb_deposits:" << _nbDeposits << std::endl;
+}
+// Render:
+//	[19920104_091532] index:0;p_amount:47;withdrawal:refused
+// 	[19920104_091532] index:1;p_amount:819;withdrawal:34;amount:785;nb_withdrawals:1
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "p_amount:" << _amount << ";";
+	std::cout << "withdrawal:";
+	if (_amount < withdrawal)
+	{
+		std::cout << "refused" << std::endl;
+		return (false);
+	}
+	std::cout << withdrawal << ";";
+	_amount -= withdrawal;
+	_totalAmount -= withdrawal;
+	std::cout << "amount:" << _amount << ";";
+	_nbWithdrawals++;
+	_totalNbWithdrawals++;
+	std::cout << "nb_withdrawals:" << _nbWithdrawals << std::endl;
+	return (true);
+}
+//-------------------------------------- FONCTION static--------------------------------------
+// static void	_displayTimestamp( void );
+// format: [19920104_091532]
+// [year/month/day __ hour/min/sec]
+// static void	_displayTimestamp( void )
+// {
+// 	const time_t	now = time(0);//temps actuel en secondes
+// 	tm				*ltm = localtime(&now);//struct qui contient le temps décomposé
+
+// 	std::cout << "[";
+//     std::cout << 1900 + ltm->tm_year; //commence a 0 en 1900, 2024 = 124
+//     std::cout << std::setfill('0') << std::setw(2) << ltm->tm_mon + 1; // +1 ,janvier = 0;
+//     std::cout << std::setfill('0') << std::setw(2) << ltm->tm_mday;
+//     std::cout << "_";
+//     std::cout << std::setfill('0') << std::setw(2) << ltm->tm_hour;
+//     std::cout << std::setfill('0') << std::setw(2) << ltm->tm_min;
+//     std::cout << std::setfill('0') << std::setw(2) << ltm->tm_sec;
+//     std::cout << "] ";
+// }
+
+//-------------------------------------- FONCTION membre--------------------------------------
+
+// int	checkAmount( void ) const;
+
+int		Account::checkAmount( void ) const
+{
+	return (_amount);
+}
