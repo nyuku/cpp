5328010ed20dfd474d2c390c53c2369067ce2f0f
Nyuk <85057677+nyuku@users.noreply.github.com>
2024-05-15 19:37:55 +0200

    Update Weapon.hpp

--- cpp01/ex03/Weapon.hpp
+++ cpp01/ex03/Weapon.hpp
@@ -1,27 +1,28 @@
 
 # ifndef WEAPON_HPP
 # define WEAPON_HPP
-# define PEACH		"\033[0;93m"
+
 
 //--------------- Library -------------------
 # include	<string>
 # include	<iostream>
 
 //--------------- Color -------------------
-#define RESET_COLOR "\033[0m"
-#define SOFT_GREEN   "\033[0;92m"
-#define LIGHT_GREEN	 "\033[1;32m"
-#define LIGHTPURPLE		" \033[1;35m"
-#define LIGHT_LILAC		" \033[1;34m"
-#define LIGHT_MAGENTA   " \033[0;95m"
-#define LIGHT_CYAN		" \033[1;36m"
-#define MAGENTA			" \033[0;35m"
-#define LILAC			" \033[0;94m"
-#define BLUE			" \033[0;34m"
-#define CYAN			" \033[0;36m"
+#define RESET_COLOR 	"\033[0m"
+#define SOFT_GREEN  	"\033[0;92m"
+#define LIGHT_GREEN	"\033[1;32m"
+#define LIGHTPURPLE	"\033[1;35m"
+#define LIGHT_LILAC	"\033[1;34m"
+#define LIGHT_MAGENTA   "\033[0;95m"
+#define LIGHT_CYAN	"\033[1;36m"
+#define MAGENTA		"\033[0;35m"
+#define LILAC		"\033[0;94m"
+#define BLUE		"\033[0;34m"
+#define CYAN		"\033[0;36m"
 #define LIGHT_RED       " \033[0;91m"
 #define GREEN           " \033[0;32m"
 #define DARK_BLUE       "\033[0;94m"
+# efine PEACH		"\033[0;93m"
 
 class Weapon
 {
@@ -35,7 +36,7 @@ class Weapon
 	// -----------------  Getters -----------------
 		const std::string	&getType(void)const;
 	// -----------------  Setters -----------------
-		void setType(std::string str);
+		void 			setType(std::string str);
 	//-------------- Fonction ----------------
 
 };
