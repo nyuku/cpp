d2d790574f86da88578b87edca428c1ae682db6e
angela <angela@MacBook-Pro-de-angela.local>
2024-05-15 19:52:00 +0200

    plop

--- cpp01/ex03/Weapon.hpp
+++ cpp01/ex03/Weapon.hpp
@@ -8,20 +8,20 @@
 # include	<iostream>
 
 //--------------- Color -------------------
-#define RESET_COLOR 	"\033[0m"
-#define SOFT_GREEN  	"\033[0;92m"
+#define RESET_COLOR		"\033[0m"
+#define SOFT_GREEN		"\033[0;92m"
 #define LIGHT_GREEN		"\033[1;32m"
 #define LIGHTPURPLE		"\033[1;35m"
 #define LIGHT_LILAC		"\033[1;34m"
-#define LIGHT_MAGENTA   "\033[0;95m"
+#define LIGHT_MAGENTA	"\033[0;95m"
 #define LIGHT_CYAN		"\033[1;36m"
 #define MAGENTA			"\033[0;35m"
 #define LILAC			"\033[0;94m"
 #define BLUE			"\033[0;34m"
-#define CYAN			"\033[0;36m"git 
-#define LIGHT_RED       "\033[0;91m"
-#define GREEN           "\033[0;32m"
-#define DARK_BLUE       "\033[0;94m"
+#define CYAN			"\033[0;36m"
+#define LIGHT_RED		"\033[0;91m"
+#define SOFT_GREEN		"\033[0;32m"
+#define DARK_BLUE		"\033[0;94m"
 #define PEACH			"\033[0;93m"
 
 class Weapon
